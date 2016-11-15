63c37e09075b48048e4ed6beef16683845beb9ed
Siddarth Kalra <sid@sidkalra.com>
2016-11-15 15:50:33 -0500

    Initial Commit

---
+++ ShiftTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShiftTracker
+//
+//  Created by Crul on 2016-11-15.
+//  Copyright © 2016 Sid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
